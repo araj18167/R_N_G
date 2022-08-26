dc2c1dfe0a90460e87bee426f78907c3dbd5b8dc
Ishan Kashyap <78429831+ishankrs@users.noreply.github.com>
2022-08-26 16:28:29 +0530

    Create generatenum.py

---
+++ generatenum.py
@@ -0,0 +1,8 @@
+import random
+
+random.randint(a,b)
+
+
+#you can use this to get a number between any two number .
+#the inital number 'a' is the number which indicates the number of which we need the higher number , any result will be greater than 'a'.
+#the 2nd number 'b' indicates the number of which the result needs to be smaller than.
